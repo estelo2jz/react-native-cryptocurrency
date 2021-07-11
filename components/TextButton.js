239c2e64dd1c128c0d22f41dfd9f66e0a37a6602
esteloabell <manitobaaa@gmail.com>
2021-07-11 18:31:33 -0500

    Create TextButton.js

---
+++ components/TextButton.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+import { dummyData, COLORS, SIZES, FONTS, icons } from '../constants';
+
+
+const TextButton = ({ label, customContainerStyle, customLabelStyle, onPress }) => {
+  return (
+    <TouchableOpacity
+      style={{
+        height: 46,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: SIZES.radius,
+        backgroundColor: COLORS.green,
+        ...customContainerStyle,
+      }}
+      onPress={onPress}
+    >
+      <Text
+        style={{
+          color: COLORS.white,
+          ...FONTS.h3,
+          ...customLabelStyle,
+        }}
+      >{label}</Text>
+    </TouchableOpacity>
+  )
+}
+
+export default TextButton
+
+const styles = StyleSheet.create({})
